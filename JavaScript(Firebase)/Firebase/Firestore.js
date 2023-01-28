ac0845d749cbd286be975fc19269cf354192ad01
DySasha <74455810+DySasha@users.noreply.github.com>
2023-01-28 14:06:30 +0200

    Add files via upload

---
+++ JavaScript(Firebase)/Firebase/Firestore.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwLkj9bCkxjO_DCVK5YSuzEC0YabKCwyw",
+  authDomain: "data-5ba58.firebaseapp.com",
+  projectId: "data-5ba58",
+  storageBucket: "data-5ba58.appspot.com",
+  messagingSenderId: "297837556550",
+  appId: "1:297837556550:web:200f5bcc18644ec90a2b47",
+  measurementId: "G-E02DC5DZ5V"
+};
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
